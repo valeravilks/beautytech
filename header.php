57e68dceff2b3dde21189eeab8511c3bcb88ef6f
valeravilks <valeravilks@gmail.com>
2021-10-01 02:46:29 +0300

    Font mixins

--- header.php
+++ header.php
@@ -26,34 +26,33 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'beautytech' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$beautytech_description = get_bloginfo( 'description', 'display' );
-			if ( $beautytech_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $beautytech_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="site-header__container">
+			<a href="<?php bloginfo('url'); ?>" class="site-header__logo logo">
+				<img src="<?php the_field('logo', 'option'); ?>" srcset="<?php the_field('logo-retina', 'option'); ?> 2x" alt="logo" class="logo__img">	
+			</a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'beautytech' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'beautytech' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav>
+			<?php 
+			$link = get_field('contact-page-link', 'option');
+			
+			if($link): ?>
+			<a 
+				href="<?php echo esc_url($link['url']) ?>" 
+				class="link link-primary"
+			>
+				<?php echo esc_html($link['title'])?>
+			</a>
+			<?php endif; ?>
+		</div>
+
 	</header><!-- #masthead -->
