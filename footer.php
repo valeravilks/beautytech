f67c911446f82c6cac63b82cdb16f3fda9bdca50
valeravilks <valeravilks@gmail.com>
2021-10-01 22:34:56 +0300

    Work

--- footer.php
+++ footer.php
@@ -12,11 +12,35 @@
 ?>
 
 	<footer id="colophon" class="site-footer footer">
+		<h2 class="h2">asdasdd</h2>
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-l-6">
+					sdf
+				</div>
+				<div class="col-12 col-l-6">
+					sdf
+				</div>
+			</div>
+		</div>
 		<div class="footer__top footer-top">
+			<div class="footer-top__row">
+				<div class="footer-top__col-1">
+					<a href="<?php bloginfo('url'); ?>" class="footer-top__logo logo">
+						<img src="<?php the_field('logo', 'option'); ?>" srcset="<?php the_field('logo-retina', 'option'); ?> 2x" alt="logo" class="logo__img">	
+					</a>
+				</div>
+				<div class="footer-top__col-2">
+					ss
+				</div>
+				<div class="footer-top__col-3">
+					ss
+				</div>
+				<div class="footer-top__col-4">
+					ss
+				</div>
+			</div>
 			
-			<a href="<?php bloginfo('url'); ?>" class="footer-top__logo logo">
-				<img src="<?php the_field('logo', 'option'); ?>" srcset="<?php the_field('logo-retina', 'option'); ?> 2x" alt="logo" class="logo__img">	
-			</a>
 		</div>
 		<div class="footer__bottom footer-bottom">
 
