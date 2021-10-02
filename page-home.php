ad0af1de1e93c2aaff8920ed5d5060aa797cc0fd
valeravilks <valeravilks@gmail.com>
2021-10-02 18:46:28 +0300

    Complete main slider

--- page-home.php
+++ page-home.php
@@ -81,6 +81,49 @@ get_header();
 								</div>
 					</div>
 				</div>
+				<div class="home-slider__slide swiper-slide" style="background-image: url('https://beautytech.com/wp-content/uploads/2021/10/Liner.jpg')">
+					<div class="home-slider__gradient">
+						<div class="container home-slider__container">
+							<div class="row g-0">
+								<div class="col-12 col-l-6 home-slider__btn">
+									<a href="#" class="home-slider__link link link-outline link-fill">
+										Learn more
+									</a>	
+								</div>
+								<div class="col-12 col-l-6 home-slider__t-o">
+									<div class="home-slider__header">
+										tri-beam premium2
+									</div>
+									<div class="home-slider__text">
+										An effective technology that allows you to treat age spots and small wrinkles. With three wavelengths, you can do most types and process pigments in tattoos effectively.
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="home-slider__element sl__element">
+									<hr class="sl__hr-1">
+									<hr class="sl__hr-2">
+								</div>
+								<div class="home-slider__element sl__element-2">
+									<div class="sl__container container">
+										<div class="sl__row row">
+											<div class="col-3">
+												<div class="sl__dot sl__dot-none"></div>	
+											</div>
+											<div class="col-3">
+												<div class="sl__dot sl__dot-none"></div>	
+											</div>
+											<div class="col-3">
+												<div class="sl__dot"></div>	
+											</div>
+											<div class="col-3">
+												<div class="sl__dot"></div>	
+											</div>
+										</div>
+									</div>
+								</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<?php get_template_part('template-parts/contact-us-block'); ?>
