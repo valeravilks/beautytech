f53aabc228973bde9743337d3b2b3f6cc7effb49
valeravilks <valeravilks@gmail.com>
2021-10-02 12:27:51 +0300

    Work with form

--- template-parts/contact-us-block.php
+++ template-parts/contact-us-block.php
@@ -4,17 +4,77 @@
       Contact us
     </h2>
     <div class="row">
-      <div class="col-12 col-l-6 contact-us-block__back">
-        <div class="contact-us-block__desc">
-          Please fill out the form and we will contsct you shortly 
-        </div>
+      <div class="col-12 col-l-6">
+        <div class="contact-us-block__back">
+          <div class="contact-us-block__desc">
+            Please fill out the form and we will contsct you shortly 
+          </div>
+        </div> 
       </div>
     </div>
     <div class="row">
-      
-      <form action="" class="contact-form">
-
-      </form>
+      <div class="col-12 col-l-6">
+        <form action="" class="contact-form">
+          <div class="row">
+            <div class="col-12">
+              <label class="contact-form__label">
+                <div class="contact-form__text">
+                  Name*
+                </div>
+                <input class="contact-form__input" type="text" placeholder="Name">
+              </label>
+            </div>
+            <div class="col-12 col-l-6">
+              <label class="contact-form__label">
+                  <div class="contact-form__text">
+                    Name*
+                  </div>
+                  <input class="contact-form__input" type="text" placeholder="Name">
+                </label>
+            </div>
+            <div class="col-12 col-l-6">
+              <label class="contact-form__label">
+                  <div class="contact-form__text">
+                    Name*
+                  </div>
+                  <input class="contact-form__input" type="text" placeholder="Name">
+                </label>
+            </div>
+            <div class="col-12">
+              <label class="contact-form__label">
+                <div class="contact-form__text">
+                  Name*
+                </div>
+                <input class="contact-form__input" type="text" placeholder="Name">
+              </label>
+            </div>
+            <div class="col-12">
+              <label class="contact-form__label">
+                <div class="contact-form__text">
+                  Name*
+                </div>
+                <textarea class="contact-form__input contact-form__textarea" placeholder="Name"></textarea>
+              </label>
+            </div>
+            <div class="col-12">
+              <div class="contact-form__checkbox">
+                <img src="<?php echo get_template_directory_uri();?>/img/svg/icon-whatsapp.svg" alt=""> 
+                <label class="contact-form__check check option">
+                  <input class="check__input" type="checkbox">
+                  <span class="check__box"></span>
+                  Please contact me by Whatsapp
+              </label>
+              </div>
+            </div>
+          </div>
+          <div class="contact-form__bottom">
+            <div class="contact-form__recaptcha">
+              recaptcha
+            </div>
+            <input class="contact-form__submit" type="submit" value="SEND">
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </div>
