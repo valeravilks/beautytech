1a7d31d77afce5bf86b7bcdba9f5e0624e43e5f4
valeravilks <valeravilks@gmail.com>
2021-10-03 21:14:24 +0300

    Home services

---
+++ template-parts/home-services.php
@@ -0,0 +1,63 @@
+<div class="home-s">
+  <?php get_template_part('template-parts/h2-block', '', array(
+        'description' => 'our services',
+        'h2' => 'Our company provides the following services',
+        'color' => 'white',
+        // 'link' => 'https://link',
+      ));
+  ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-l-6">
+        <div class="home-s__block home-s__mob">
+          <div class="home-s__block-2">
+            <div class="home-s__title">
+              Repair service
+            </div>
+            <a href="#" class="home-s__link">
+              <svg class="home-s__link-svg" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 45L45 1M45 1H1M45 1V45" stroke="#FEFEFE" stroke-width="2"/>
+              </svg>
+            </a>
+          </div>
+          <div class="home-s__text">
+            <p class="home-s__p">
+              In 2009 our workshop was rebuilt and enlarged. We competently carry out repairs, assembly and individual orders.
+            </p>
+            <p class="home-s__p">
+              We offer you various customer services, from standard service to premium customer service for IPL and cosmetic devices.
+            </p>
+          </div>
+          <a href="#" class="home-s__link link link-plus">
+            Learn more
+          </a>
+        </div>
+      </div>
+      <div class="col-12 col-l-6">
+        <div class="home-s__block">
+          <div class="home-s__block-2">
+            <div class="home-s__title">
+              Repair service
+            </div>
+            <a href="#" class="home-s__link">
+              <svg class="home-s__link-svg" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1 45L45 1M45 1H1M45 1V45" stroke="#FEFEFE" stroke-width="2"/>
+              </svg>
+            </a>
+          </div>
+          <div class="home-s__text">
+            <p class="home-s__p">
+              In 2009 our workshop was rebuilt and enlarged. We competently carry out repairs, assembly and individual orders.
+            </p>
+            <p class="home-s__p">
+              We offer you various customer services, from standard service to premium customer service for IPL and cosmetic devices.
+            </p>
+          </div>
+          <a href="#" class="home-s__link link link-plus">
+            Learn more
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
