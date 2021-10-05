6f37efd30daa3afd445396e258f45e99c9a6a939
valeravilks <valeravilks@gmail.com>
2021-10-05 15:39:20 +0300

    Add link and work with product cart

--- template-parts/home-products.php
+++ template-parts/home-products.php
@@ -26,7 +26,7 @@
           <div class="home-pr-1__text">
             An effective technology that allows you to treat age spots and small wrinkles. With three wavelengths, you can do most types and process pigments in tattoos effectively.
           </div>
-          <a href="#" class="home-pr-1__link link link-primary">Explore about product</a>
+          <a href="#" class="home-pr-1__link link link-plus-2">Explore about product</a>
         </div>
       </div>
       <div class="col-12 col-l-6">
@@ -42,7 +42,7 @@
           <div class="home-pr-1__text">
             An effective technology that allows you to treat age spots and small wrinkles. With three wavelengths, you can do most types and process pigments in tattoos effectively.
           </div>
-          <a href="#" class="home-pr-1__link link link-primary">Explore about product</a>
+          <a href="#" class="home-pr-1__link link link-plus-2">Explore about product</a>
         </div>
       </div>
       <div class="col-12 col-l-6">
@@ -58,7 +58,7 @@
           <div class="home-pr-1__text">
             An effective technology that allows you to treat age spots and small wrinkles. With three wavelengths, you can do most types and process pigments in tattoos effectively.
           </div>
-          <a href="#" class="home-pr-1__link link link-primary">Explore about product</a>
+          <a href="#" class="home-pr-1__link link link-plus-2">Explore about product</a>
         </div>
       </div>
     </div>
@@ -66,7 +66,22 @@
       <div class="home-pr-2__row row g-0">
         <div class="col-12 col-l-9">
           <div class="home-pr-2__right">
-            sdf
+            <div class="home-pr-2__text">
+              Exceptional clinical performance with excellent results
+            </div>
+            <img src="https://beautytech.com/wp-content/uploads/2021/10/Tribeam-Logo.png" alt="" class="home-pr-2__logo"> 
+            <div class="home-pr-2__blink">
+              <a href="#" class="home-pr-2__link link link-plus-2">
+                Explore about product
+              </a> 
+            </div>  
+            <div class="home-pr-2__title">
+              EDGE ONE
+            </div>
+            <img src="https://beautytech.com/wp-content/uploads/2021/10/Слой-3-1.png" alt="" class="home-pr-2__img">  
+            <div class="home-pr-2__desc">
+              As a high-performance system with numerous treatment options, Edge ONE is suitable for modern dermatology, aesthetics and surgery.
+            </div>
           </div>
         </div>
         <div class="col-12 col-l-3">
