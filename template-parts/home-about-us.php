080c389beca2ae4961be34141029c1b21badbfa1
valeravilks <valeravilks@gmail.com>
2021-10-06 16:35:31 +0300

    Acf for about home

--- template-parts/home-about-us.php
+++ template-parts/home-about-us.php
@@ -1,68 +1,73 @@
-<?php get_template_part('template-parts/h2-block', '', array(
-			'description' => 'About us',
-			'h2' => 'Welcome to our website',
+<?php 
+
+get_template_part('template-parts/h2-block', '', array(
+			'description' => get_field('home-setting-about-subtitle', 'option'),
+			'h2' => get_field('home-setting-about-title', 'option'),
 			// 'color' => 'white',
-			'link' => 'https://link',
+			'link' => get_field('home-setting-about-link', 'option'),
 		));
 ?>
 <div class="home-about-us">
   <div class="container">
     <div class="row home-about-us__block-1">
       <div class="col-12 col-l-6 home-about-us__text">
-        BeautyTECH supports and supplies customers worldwide, it is not a goal, but an achievement, on every continent we can give customers as references. The motivation of our customers to make the decision for us is our international know-how, honesty and passion for our work.
+        <?php the_field('home-setting-about-link-text-1', 'option')?>
       </div>
       <div class="col-12 col-l-6 home-about-us__text">
-        Because we offer excellent quality, innovative products and services. From plasmapen to diode lasers, the latest technologies are available from us at any time.
+        <?php the_field('home-setting-about-link-text-2', 'option')?>
       </div>
     </div>
     <div class="row home-about-us__block-2">
       <div class="col-12 col-l-6 home-about-us__text-2">
-          We have many years of wide-ranging expertise and a network that allows us to identify the right and current technologies
-        </div>
+        <?php the_field('home-setting-about-link-text-3', 'option')?>
+      </div>
     </div>
     <div class="row home-about-us__block-3">
       <div class="col-12 col-l-6">
-        <img class="home-about-us__img" src="https://beautytech.com/wp-content/uploads/2021/10/Image.jpg" alt="image">
+        <img class="home-about-us__img" src="<?php echo esc_url(get_field('home-setting-about-image', 'option'))?>" alt="image">
       </div>
-      <div class="col-12 col-l-6 home-about-us__block-5">
-        <hr class="home-about-us__hr">
-        <div class="row home-about-us__block-4">
-          <div class="col-12 col-l-6 home-about-us__text-3">
-            IPL systems tailored to your needs:
+      <?php $texts = get_field('home-setting-about-info', 'option'); 
+        if($texts) :?>
+        <div class="col-12 col-l-6 home-about-us__block-5">
+          <hr class="home-about-us__hr">
+          <div class="row home-about-us__block-4">
+            <div class="col-12 col-l-6 home-about-us__text-3">
+              <?php echo $texts['subtitle-1']; ?>
+            </div>
+            <div class="col-12 col-l-6 home-about-us__text-4">
+              <?php echo $texts['text-1']; ?>
+            </div>
           </div>
-          <div class="col-12 col-l-6 home-about-us__text-4">
-            Permanent hair removal or skin treatments
+          <hr class="home-about-us__hr">
+          <div class="row home-about-us__block-4">
+            <div class="col-12 col-l-6 home-about-us__text-3">
+              <?php echo $texts['subtitle-2']; ?>
+            </div>
+            <div class="col-12 col-l-6 home-about-us__text-4">
+              <?php echo $texts['text-2']; ?>
+            </div>
           </div>
+          <hr class="home-about-us__hr">
+          <div class="row home-about-us__block-4">
+            <div class="col-12 col-l-6 home-about-us__text-3">
+              <?php echo $texts['subtitle-3']; ?>
+            </div>
+            <div class="col-12 col-l-6 home-about-us__text-4">
+              <?php echo $texts['text-3']; ?>
+            </div>
+          </div>
+          <hr class="home-about-us__hr">
+          <div class="row home-about-us__block-4">
+            <div class="col-12 col-l-6 home-about-us__text-3">
+              <?php echo $texts['subtitle-4']; ?>
+            </div>
+            <div class="col-12 col-l-6 home-about-us__text-4">
+              <?php echo $texts['text-4']; ?>
+            </div>
+          </div>
+          <hr class="home-about-us__hr">
         </div>
-        <hr class="home-about-us__hr">
-        <div class="row home-about-us__block-4">
-          <div class="col-12 col-l-6 home-about-us__text-3">
-          Laser technologies:
-          </div>
-          <div class="col-12 col-l-6 home-about-us__text-4">
-          CO2, Nd: YAG, diode laser
-          </div>
-        </div>
-        <hr class="home-about-us__hr">
-        <div class="row home-about-us__block-4">
-          <div class="col-12 col-l-6 home-about-us__text-3">
-          RF technologies: 
-          </div>
-          <div class="col-12 col-l-6 home-about-us__text-4">
-          High Frequency, Low Frequency, Non-Ablative, Invasive, Non-invasive
-          </div>
-        </div>
-        <hr class="home-about-us__hr">
-        <div class="row home-about-us__block-4">
-          <div class="col-12 col-l-6 home-about-us__text-3">
-          Meso:
-          </div>
-          <div class="col-12 col-l-6 home-about-us__text-4">
-          Dermal Needle Pen, Microneedle Pen, Dermaroller
-          </div>
-        </div>
-        <hr class="home-about-us__hr">
-      </div>
+        <?php endif;?>
     </div>
     <div class="home-about-us__block-6">
       <a href="#" class="home-about-us__link link link-plus">Learn more</a>
