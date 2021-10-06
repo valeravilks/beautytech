224e74eecec34bbabdf89015d88cfd5bf5c15d6c
valeravilks <valeravilks@gmail.com>
2021-10-07 01:06:00 +0300

    Breadcrumbs style

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -1,5 +1,9 @@
-<?php
-if ( function_exists('yoast_breadcrumb') ) {
-  yoast_breadcrumb( '<p class="breadcrumbs">','</p>' );
-}
-?>
+<div class="breadcrumbs">
+  <div class="container">
+    <?php
+    if ( function_exists('yoast_breadcrumb') ) {
+      yoast_breadcrumb( '<p class="breadcrumbs__link">','</p>' );
+    }
+    ?>
+  </div>
+</div> 
