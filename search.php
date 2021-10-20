b61556c3c1d7e291ca591d0a3076389e3fc0c732
valeravilks <valeravilks@gmail.com>
2021-10-20 12:19:33 +0300

    404 page work

--- search.php
+++ search.php
@@ -15,39 +15,19 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'beautytech' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
+				<div class="container">
+					<h1 class="page-title">
+						<?php
+						/* translators: %s: search query. */
+						printf( esc_html__( 'Search Results for: %s', 'beautytech' ), '<span>' . get_search_query() . '</span>' );
+						?>
+					</h1>
+				</div>
 			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+			<?php endif; ?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
+?>
