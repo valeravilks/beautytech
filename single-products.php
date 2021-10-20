b446a9f53bcd9718acf7ed037e7d99f30d16f397
valeravilks <valeravilks@gmail.com>
2021-10-20 12:46:20 +0300

    Product faq fix padding

--- single-products.php
+++ single-products.php
@@ -12,7 +12,6 @@
 		<?php get_template_part('template-parts/product', 'testimonail'); ?>
 		<?php get_template_part('template-parts/product', 'video'); ?>
 		<?php get_template_part('template-parts/product', 'faq'); ?>
-		<?php get_template_part('template-parts/home', 'support');?>
 		<?php get_template_part('template-parts/contact-us-block'); ?>
 		<?php get_template_part('template-parts/company-list'); ?>
 	</main>
