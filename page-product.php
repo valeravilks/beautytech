aa19994f9512d3b3aaf31a002f5a53d52f1f35fe
valeravilks <valeravilks@gmail.com>
2021-12-07 14:22:46 +0300

    Block 3

--- page-product.php
+++ page-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -37,58 +38,93 @@ $bl20 = get_field('pr-block20');
 
 ?>
 
-	<main id="primary" class="site-main page-product pp">
-		<?php if($bl1)?>
-		<section class="pp-b1">
-			<div class="container">
-				<div class="row">
-					<div class="col-12 offset-l-5 col-l-6">
-						<img src="<?php echo $bl1['logo']['url'];?>" class="pp-b1-1">
-					</div>
+<main id="primary" class="site-main page-product pp">
+	<?php if ($bl1) ?>
+	<section class="pp-b1">
+		<div class="container">
+			<div class="row">
+				<div class="col-12 offset-l-5 col-l-6">
+					<img src="<?php echo $bl1['logo']['url']; ?>" class="pp-b1-1">
 				</div>
-				<div class="pp-b1-2">
-					<div class="row pp-b1-9">
-						<div class="col-12 offset-l-6 col-l-6 pp-b1-7">
-							<div class="pp-b1-3">
-								<?php echo $bl1['title'];?>
-							</div>
-							<div class="pp-b1-4">
-								<?php echo $bl1['text'];?>
-							</div>
-							<a href="#" class="pp-b1-5 js-consultation">
-								<?php echo $bl1['button'];?>
-							</a>
-							<img src="<?php echo $bl1['image']['url']?>" alt="img" class="pp-b1-6">
-							<div class="pp-b1-8">
-								<?php echo $bl1['name'];?>
-							</div>
+			</div>
+			<div class="pp-b1-2">
+				<div class="row pp-b1-9">
+					<div class="col-12 offset-l-6 col-l-6 pp-b1-7">
+						<div class="pp-b1-3">
+							<?php echo $bl1['title']; ?>
+						</div>
+						<div class="pp-b1-4">
+							<?php echo $bl1['text']; ?>
+						</div>
+						<a href="#" class="pp-b1-5 js-consultation">
+							<?php echo $bl1['button']; ?>
+						</a>
+						<img src="<?php echo $bl1['image']['url'] ?>" alt="img" class="pp-b1-6">
+						<div class="pp-b1-8">
+							<?php echo $bl1['name']; ?>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section>
-		<section class="pp-b2">
-			<div class="container">
-				<div class="row g-0">
-					<div class="col-12 col-l-6 pp-b2-5">
-						<div class="pp-b2-1">
-							<?php echo $bl2['name'];?>
-						</div>
-						<img src="<?php echo $bl2['image']['url'];?>" class="pp-b2-2">
+		</div>
+	</section>
+	<section class="pp-b2">
+		<div class="container">
+			<div class="row g-0">
+				<div class="col-12 col-l-6 pp-b2-5">
+					<div class="pp-b2-1">
+						<?php echo $bl2['name']; ?>
 					</div>
-					<div class="col-12 col-l-6">
-						<div class="pp-b2-3">
-							<?php echo $bl2['title'];?>
-						</div>
-						<div class="pp-b2-4">
-							<?php echo $bl2['text'];?>
-						</div>
+					<img src="<?php echo $bl2['image']['url']; ?>" class="pp-b2-2">
+				</div>
+				<div class="col-12 col-l-6">
+					<div class="pp-b2-3">
+						<?php echo $bl2['title']; ?>
+					</div>
+					<div class="pp-b2-4">
+						<?php echo $bl2['text']; ?>
 					</div>
 				</div>
 			</div>
-		</section>
-		
-	</main><!-- #main -->
+		</div>
+	</section>
+	<section class="pp-b3">
+		<?php get_template_part('template-parts/h2-block', '', array(
+			'h2' => $bl3['title'],
+		));
+		?>
+		<div class="container">
+			<div class="row">
+				<?php
+				$index = 0;
+				$num = 3;
+				// var_dump($bl3['items']);
+				foreach ($bl3['items'] as $element) : ?>
+					<div class="col-12 col-l-4">
+						<div class="pp-b3__block">
+							<div class="pp-b3__sub">
+								<?php echo $element['title']; ?>
+							</div>
+							<div class="pp-b3__text">
+								<?php echo $element['text']; ?>
+							</div>
+							<hr class="pp-b3__hr <?php
+																		if ($index > count($bl3['items']) - 1 - $num) {
+																			echo 'pp-b3__hr2';
+																		}
+																		if ($index == count($bl3['items']) - 1) {
+																			echo ' pp-b3__hr3';
+																		}
+																		?>">
+						</div>
+					</div>
+					<?php
+					$index++; ?>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</section>
+</main><!-- #main -->
 <?php
 get_footer();
 
